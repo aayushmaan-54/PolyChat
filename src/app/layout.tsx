e58df4a29f0723c8ab4f4105283b4e69fc183c51
Aayushmaan Soni <aayushmaan.soni54@gmail.com>
2025-02-27 23:31:43 +0530

    Added favicon and opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,30 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
-
 export const metadata: Metadata = {
-  title: "",
-  description: "",
+  metadataBase: new URL("https://poly-chat.vercel.app"),
+  title: {
+    default: "Polychat - AI Chat with Custom Models",
+    template: "Polychat | %s",
+  },
+  description: "Polychat is a powerful AI chat app that lets you integrate various AI models, customize settings, and create expert AI personalities with custom prompts.",
+  icons: {
+    icon: "/polychat.svg",
+  },
+  openGraph: {
+    title: "Polychat - AI Chat with Custom Models",
+    description: "Chat with AI like never before! Use multiple AI models, customize responses, and create expert AI personas tailored to your needs.",
+    images: ["/polychat-og.png"],
+  },
+  keywords: [
+    "AI Chatbot",
+    "Polychat",
+    "Custom AI Models",
+    "ChatGPT Alternative",
+    "AI Conversations",
+    "AI Expert Models",
+    "AI API Integration",
+  ],
 };
 
 
@@ -16,7 +35,7 @@ export default function RootLayout({ children}:
 ) {
   return (
     <html lang="en">
-      <body className={``}>
+      <body>
         {children}
       </body>
     </html>
